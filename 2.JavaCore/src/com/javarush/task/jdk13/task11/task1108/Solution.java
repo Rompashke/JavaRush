ea16312ef6a746b1109da74733e439582bab2ed0
Roman Sattarov <enomine_90@mail.ru>
2022-05-18 06:20:06 +0500

    Module 2. Java Core 3 level, 0 lecture was completed. Module 2. Java Core 3 level, 1 lecture task 1112 was completed.

---
+++ 2.JavaCore/src/com/javarush/task/jdk13/task11/task1108/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.jdk13.task11.task1108;
+
+/* 
+Играем в Дарвина
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Fish {
+
+    }
+
+    public class Animal extends Fish {
+
+    }
+
+    public class Ape extends Animal {
+
+    }
+
+    public class Human extends Ape {
+
+    }
+
+}
+
+/*
+5.	Класс Animal должен быть унаследован от класса Fish.
+6.	Класс Ape должен быть унаследован от класса Animal.
+7.	Класс Human должен быть унаследован от класса Ape.
+ */
