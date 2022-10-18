1f0bf84113480b23e23e81cb6b7bed6b7d80fe98
Roman Sattarov <enomine_90@mail.ru>
2022-10-18 07:05:32 +0500

    Module 2. Java Core 14 level, lecture 5, task 2817 was complete.

--- 2.JavaCore/src/com/javarush/task/jdk13/task28/task2817/Solution.java
+++ 2.JavaCore/src/com/javarush/task/jdk13/task28/task2817/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.jdk13.task28.task2817;
 
-import java.util.Date;
+
+import java.time.LocalTime;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -14,7 +15,9 @@ public class Solution {
     public static ScheduledExecutorService scheduledPool = Executors.newScheduledThreadPool(3);
 
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        scheduledPool.scheduleAtFixedRate(() ->
+            System.out.println(LocalTime.now())
+        , 0 , 500, TimeUnit.MILLISECONDS);
 
 
         Thread.sleep(3000);
